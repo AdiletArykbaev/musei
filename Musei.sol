30475ea62edb648cff9093317830377c66f72fab
kurmanbek12 <92358351+kurmanbek12@users.noreply.github.com>
2022-07-30 15:24:47 +0600

    added functional component

--- Musei.sol
+++ Musei.sol
@@ -1,9 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
 
 contract Musei{
-   IERC1155 public parentNFT;
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event StartAuction();
+    event Bid(address indexed sender,uint amount);
+    event Withdraw(address indexed bidder,uint amount);
+    event End(address highestBidder,uint amount);
+
+   IERC1155 public immutable parentNFT;
+   uint public immutable nftId;
+   address private _owner;
+   address payable public seller;
+   uint32 public endAt;
+   bool public started; 
+   bool public ended;
+
+   address public highestBidder;
+   uint public highestBid;
+
+   mapping(address => uint) public bids; 
    mapping(address => uint256) public stakingTime;    
    mapping(address => Stake) public stakes;
 
@@ -12,10 +33,66 @@ contract Musei{
         uint256 amount;
         uint256 timestamp;
     }
-   constructor() public{   
-        
-    }
+  constructor () {
+    address msgSender = msg.sender;
+    _owner = msgSender;
+    emit OwnershipTransferred(address(0), msgSender);
+  }
   
+   function auctionData(address _nft,uint _nftId,uint _startingBid) public payable {
+       parentNFT = IERC1155(_nft);
+       nftId = _nftId;
+       seller = payable(msg.sender);
+       highestBid = _startingBid;
+   }
+   function startAuction() external {
+       require(msg.sender == seller, "Not seller");
+       require(!started,"Started");
+
+       started = true;
+       endAt = uint32(block.timestamp + 1 days);
+       parentNFT.transferFrom(seller, address(this), nftId);
+
+       emit StartAuction();
+   }
+   function endAuction() external {
+       require(started,"Not started");
+       require(!ended,"Ended");
+       require(block.timestamp >=endAt,"Not ended");
+
+       ended = true;
+       if(highestBidder != address(0)) {
+           parentNFT.transferFrom(address(this),highestBidder,nftId);
+           seller.transfer(highestBid);
+       } else {
+           parentNFT.transferFrom(address(this),seller,nftId);
+       }
+       emit End(highestBidder,highestBid);
+   }
+
+   function bid() external payable {
+       require(started,"Not started");
+       require(block.timestamp < endAt,"Ended");
+       require(msg.value > highestBid,"little bid");
+
+        if(highestBidder != address(0)) {
+            bids[highestBidder] += highestBid;
+        }
+
+        highestBid = msg.value;
+        highestBidder = msg.sender;
+
+        emit Bid(msg.sender,msg.value);
+   }
+
+    function withdraw() external {
+        uint uBid = bids[msg.sender];
+        bids[msg.sender] = 0;
+        payable(msg.sender).transfer(uBid);
+
+        emit Withdraw(msg.sender,uBid);
+    }
+
    function paytosmart() public payable{
       
    }
